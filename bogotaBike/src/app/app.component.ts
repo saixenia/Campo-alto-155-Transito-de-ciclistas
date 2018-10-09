42c7c80231727fc8659246b11e64863b08a9c0a2
diana123 <loribeth90@hotmail.com>
2018-10-08 22:28:44 -0500

    agregando tabs

--- bogotaBike/src/app/app.component.ts
+++ bogotaBike/src/app/app.component.ts
@@ -4,12 +4,12 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 
-import { WelcomePage } from '../pages/welcome/welcome';
+import { TabsPage } from '../pages/tabs/tabs';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = WelcomePage;
+  rootPage:any = TabsPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
---
+++ bogotaBike/src/pages/tabs/tabs.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { RutaPage } from '../ruta/ruta';
+import { PerfilPage } from '../perfil/perfil';
+import { TipsPage } from '../tips/tips';
+import { BeneficiosPage } from '../beneficios/beneficios';
+import { MapaPage } from '../mapa/mapa';
+
+@Component({
+  selector: 'page-tabs',
+  templateUrl: 'tabs.html',
+})
+export class TabsPage {
+
+  rutaPage =  RutaPage;
+  perfilPage = PerfilPage;
+  tipsPage =   TipsPage;
+  beneficiosPage =  BeneficiosPage;
+  mapaPage =  MapaPage;
+
+  constructor(){
+    
+  }
+
+}
